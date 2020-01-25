97e93431dfec5985ca587da88b6641404c40444b
Eugeniu Stina <estina@c3r3s1.42madrid.com>
2020-01-25 20:31:05 +0100

    some

--- setup.sh
+++ setup.sh
@@ -1,24 +1,43 @@
 # brew install minikube
-# brew install helm
-# helm repo add stable https://kubernetes-charts.storage.googleapis.com
-# minikube service <name>
+# brew install jq
+# kubectl delete daemonsets,replicasets,services,deployments,pods,rc,pv,pvc,secrets --all
+# minikube service list
 
-minikube start --cpus=4 --memory 4000 --disk-size 11000 --vm-driver virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+#minikube start --cpus=4 --memory 4000 --disk-size 5000 --vm-driver virtualbox --extra-config=apiserver.service-node-port-range=1-35000
 
-helm repo update
+# Script by https://github.com/nicokla for waiting while the POD is not ready
+wait_for_deploy () {
+	printf "\e[0;34mWaiting for \e[0;35m"$1"\e[0;34m...\e[0m"
+	sleep 5s
+	MYSQL=$(kubectl get pod mysql-0)
+	RES="Error from server (NotFound): pods \"mysql\" not found"
+	while [ $MYSQL == $RES ] do
+		printf "\e[0;34m.\e[0m"
+		sleep 5s
+		MYSQL=$(kubectl get pod mysql-0)
+	done
+	sleep 5s
+	printf "\e[0;34mDONE\e[0m\n"
+}
 
-helm install -f srcs/influxdb.yaml influxdb stable/influxdb
-helm install -f srcs/grafana.yaml grafana stable/grafana
-
+# Activate adddons
+minikube addons enable metrics-server
 minikube addons enable ingress
 
-kubectl apply -f srcs/mysql.yaml
-kubectl apply -f srcs/wordpress.yaml
-kubectl apply -f srcs/nginx.yaml
-kubectl apply -f srcs/ingress.yaml
+# Change the IP variable to an actual kubernetes' IP in SQL file
+MINIKUBE_IP=$(minikube ip)
+cp srcs/db/mysql.sql srcs/db/mysql-target.sql
+sed -i '' "s/##MINIKUBE_IP##/$MINIKUBE_IP/g" srcs/db/mysql-target.sql
 
-kubectl get all
-minikube ip
+# Apply all Deployments, Services and Volume Claims
+kubectl apply -k srcs
 
-eval $(minikube docker-env)
+# Wait while My_SQL's POD is not ready
+# export MYSQL_POD=$(kubectl get pods | grep mysql | cut -d" " -f1)
+wait_for_deploy mysql
+
+# Execute the command to import SQL into the POD
+kubectl exec -it mysql-0 -- mysql -u root -ppassword < srcs/db/mysql-target.sql
+
+# Open Kubernetes' Dashboard
 minikube dashboard
