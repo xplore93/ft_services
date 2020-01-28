796ca882ea86d681b03c34064d22dad87d0c5138
Eugeniu Stina <estina@Eugenius-MacBook-Pro.local>
2020-01-28 18:32:55 +0100

    New version

--- setup.sh
+++ setup.sh
@@ -1,43 +1,15 @@
-# brew install minikube
-# brew install jq
-# kubectl delete daemonsets,replicasets,services,deployments,pods,rc,pv,pvc,secrets --all
-# minikube service list
+#kubectl delete "$(kubectl api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all
 
-#minikube start --cpus=4 --memory 4000 --disk-size 5000 --vm-driver virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
+kubectl apply -f srcs/ingress.yaml
 
-# Script by https://github.com/nicokla for waiting while the POD is not ready
-wait_for_deploy () {
-	printf "\e[0;34mWaiting for \e[0;35m"$1"\e[0;34m...\e[0m"
-	sleep 5s
-	MYSQL=$(kubectl get pod mysql-0)
-	RES="Error from server (NotFound): pods \"mysql\" not found"
-	while [ $MYSQL == $RES ] do
-		printf "\e[0;34m.\e[0m"
-		sleep 5s
-		MYSQL=$(kubectl get pod mysql-0)
-	done
-	sleep 5s
-	printf "\e[0;34mDONE\e[0m\n"
-}
+docker stack deploy --compose-file docker-compose.yml ft_services
 
-# Activate adddons
-minikube addons enable metrics-server
-minikube addons enable ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
 
-# Change the IP variable to an actual kubernetes' IP in SQL file
-MINIKUBE_IP=$(minikube ip)
-cp srcs/db/mysql.sql srcs/db/mysql-target.sql
-sed -i '' "s/##MINIKUBE_IP##/$MINIKUBE_IP/g" srcs/db/mysql-target.sql
+# kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
 
-# Apply all Deployments, Services and Volume Claims
-kubectl apply -k srcs
+open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 
-# Wait while My_SQL's POD is not ready
-# export MYSQL_POD=$(kubectl get pods | grep mysql | cut -d" " -f1)
-wait_for_deploy mysql
-
-# Execute the command to import SQL into the POD
-kubectl exec -it mysql-0 -- mysql -u root -ppassword < srcs/db/mysql-target.sql
-
-# Open Kubernetes' Dashboard
-minikube dashboard
+kubectl proxy
---
+++ srcs/wordpress/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpress');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'mysql:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '24e3a48f77f34fbf5d47f427e5ebc240cf10ddb2');
+define( 'SECURE_AUTH_KEY',  '82ee9909f2c2d2c9293689fe35f068dc0deb12bc');
+define( 'LOGGED_IN_KEY',    '355751a32063ee1454b46a21b629d99c48c335ff');
+define( 'NONCE_KEY',        '74e4a3281048e8e0edabd02a85c38714051a0b50');
+define( 'AUTH_SALT',        'b6e8cc7dcfd8411967b35759e88a52331beb12d5');
+define( 'SECURE_AUTH_SALT', '08e77619e7a752d6fe845712f7aa641efc5a1bc9');
+define( 'LOGGED_IN_SALT',   '22538e3a71dbb2e3f482358f14c605bc50517b15');
+define( 'NONCE_SALT',       '4337c879baa1a5438942603dedd07018521e2669');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
